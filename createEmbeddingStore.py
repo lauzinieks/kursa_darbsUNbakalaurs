9527bdfaebaf3893c3b899eaf97881e402214160
lauzinieks <rihards.hohbergs@gmail.com>
2024-04-28 19:43:42 +0300

    alloyDB

--- createEmbeddingStore.py
+++ createEmbeddingStore.py
@@ -1,14 +1,20 @@
 # Imports the Google Cloud client library
-from google.cloud import storage
 from vertexai.language_models import TextEmbeddingModel
 from langchain_community.document_loaders import SeleniumURLLoader
 from langchain_text_splitters import CharacterTextSplitter
+from langchain_google_alloydb_pg import AlloyDBEngine
+from langchain_google_vertexai import VertexAIEmbeddings
+from langchain_google_alloydb_pg import AlloyDBVectorStore
+import uuid
 
+PROJECT_ID = "lumibakalaurs"  # @param {type:"string"}
+REGION = "europe-north1"  # @param {type: "string"}
+CLUSTER = "lumicluster"  # @param {type: "string"}
+INSTANCE = "lumibachelortest3"  # @param {type: "string"}
+DATABASE = "postgres"  # @param {type: "string"}
+TABLE_NAME = "vectorembeddings"  # @param {type: "string"}
 
-def createEmbeddingStore():
-    
-    # Instantiates a client
-    storage_client = storage.Client()
+def createEmbeddingStore(_):
 
     urls = ["https://www.lu.lv/studijas/studiju-celvedis/akademiskais-kalendars/2023/2024-akademiska-gada-kalendars/", 
                 "https://www.lu.lv/studijas/studiju-programmas/bakalaura-limena-studijas/datorzinatnes/",
@@ -16,15 +22,37 @@ def createEmbeddingStore():
     
     loader = SeleniumURLLoader(urls=urls)
     documents = loader.load()
+    docs = []
+    for d in documents:
+        docs += [d.page_content]
 
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-    docs = text_splitter.split_documents(documents)
+    # text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    # docs = text_splitter.split_documents(docs1)
 
-    bucket = storage_client.bucket('lumiembeddingstorage')
-    blob = bucket.blob('embeddings.json')
+    engine = AlloyDBEngine.from_instance(
+        project_id=PROJECT_ID,
+        region=REGION,
+        cluster=CLUSTER,
+        instance=INSTANCE,
+        database=DATABASE,
+    )
 
-    model = TextEmbeddingModel.from_pretrained("textembedding-gecko@001")
-    embeddings = model.get_embeddings([docs])
-    
-    with open(embeddings, 'rb') as f:
-        blob.upload_from_file(f)
+    engine.init_vectorstore_table(
+        table_name=TABLE_NAME,
+        vector_size=768,  # Vector size for VertexAI model(textembedding-gecko@latest)
+    )
+
+    embedding = VertexAIEmbeddings(
+        model_name="textembedding-gecko@latest", project=PROJECT_ID
+    )
+
+    store = AlloyDBVectorStore.create(
+        engine=engine,
+        table_name=TABLE_NAME,
+        embedding_service=embedding,
+    )
+
+    metadatas = [{"len": len(t)} for t in docs]
+    ids = [str(uuid.uuid4()) for _ in docs]
+
+    store.add_texts(docs, metadatas=metadatas, ids=ids)
