05c86d6571f75bfac7ca4a4abe64928fbcba87cf
lauzinieks <rihards.hohbergs@gmail.com>
2024-05-08 22:54:04 +0300

    extension_v1

--- extension/contentScript.js
+++ extension/contentScript.js
@@ -1,3 +1,14 @@
-(() => {
-    let youtubeLeftControls, youtubePlayer;
-})();
+// Wait for the DOM to fully load
+window.addEventListener('DOMContentLoaded', (event) => {
+    let footerMenu = document.querySelector('ul.footer_menu'); // Targeting the specific element
+    if (footerMenu) {
+        let newListItem = document.createElement('li'); // Creating a new list item
+        let inputElement = document.createElement('input'); // Creating the input element
+        inputElement.type = 'text';
+        inputElement.placeholder = 'Enter something...';
+        newListItem.appendChild(inputElement); // Appending the input to the new list item
+        footerMenu.appendChild(newListItem); // Appending the new list item to the UL
+    } else {
+        console.log("Could not find the footer menu.");
+    }
+});
